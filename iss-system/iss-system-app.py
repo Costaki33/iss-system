3cea93309aad17dec5cd6cbd9c8553f4afa3389a
Costaki <cskevofilax@utexas.edu>
2022-03-20 17:39:49 -0500

    update

--- iss-system/iss-system-app.py
+++ iss-system/iss-system-app.py
@@ -5,9 +5,27 @@ import logging
 import socket
 
 
+#Information on how to interact with the application
+#All Epochs in the positional data
+#All information about a specific Epoch in the positional data
+#All Countries from the sighting data
+#All information about a specific Country in the sighting data
+#All Regions associated with a given Country in the sighting data
+#All information about a specific Region in the sighting data
+#All Cities associated with a given Country and Region in the sighting data
+#All information about a specific City in the sighting data
+
 app = Flask(__name__)
 
+@app.route('/download_data', methods= ['POST'])
+def download_data(); 
+    global sighting_data
+    global positional_data
 
+with open('XMLsightingData_citiesUSA10.xml', 'r') as f:
+    sighting_data = xmltodict.parse(f.read())
+
+with open()
 
 
 if __name__ == '__main__':
